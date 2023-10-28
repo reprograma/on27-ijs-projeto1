0a25630b96a2ec6ff43aae1d77ddf781df12ec2f
Heloiza Mendes <helocursos@gmail.com>
2023-10-28 17:22:45 -0300

    refatoração

--- exercicios/projeto/cliente/Cliente.js
+++ exercicios/projeto/cliente/Cliente.js
@@ -5,13 +5,6 @@ class Cliente {
     #renda;
     #conta;
 
-    // constructor(name, cpf, renda, conta) {
-    //     this.name = name;
-    //     this.#cpf = cpf;
-    //     this.#renda = renda;
-    //     this.#conta = conta;
-    // }
-
     registrar(name, cpf, renda, conta) {
         if (conta instanceof Conta) {
             this.name = name;
