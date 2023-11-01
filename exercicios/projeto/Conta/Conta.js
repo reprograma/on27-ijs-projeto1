2eebbc90037a96bc111637f3eba6577921d17691
Roxanie Fernandes <roxaniedmfernandes@gmail.com>
2023-10-31 22:15:53 -0300

    desafio parte 2

--- exercicios/projeto/Conta/Conta.js
+++ exercicios/projeto/Conta/Conta.js
@@ -3,6 +3,7 @@ class Conta {
   #conta;
   #saldo;
   chavesPix;
+  static listaContas = []
 
   constructor(agencia, conta, saldo) {
     this.#agencia = agencia;
@@ -13,6 +14,12 @@ class Conta {
       email: undefined,
       telefone: undefined,
     };
+    Conta.listaContas.push(this)
+  }
+
+  destruir(){
+    let i = Conta.listaContas.indexOf(this)
+    Conta.listaContas.splice(i, 1)
   }
 
   get getAgencia() {
@@ -31,6 +38,42 @@ class Conta {
     this.#saldo = newSaldo;
   }
 
+  criarChavePix(chavePix, tipo) {
+    switch (tipo) {
+      case "CPF":
+        let regexCPF =
+          /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+        if (regexCPF.test(chavePix)) {
+          this.chavesPix.cpf = chavePix;
+          return "Chave Pix CPF criada com sucesso";
+        } else {
+          throw new Error("Erro: CPF Inválido");
+        }
+
+      case "EMAIL":
+        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (regexEmail.test(chavePix)) {
+          this.chavesPix.email = chavePix;
+          return "Chave Pix EMAIL criada com sucesso";
+        } else {
+          throw new Error("Erro: email Inválido");
+        }
+
+      case "TELEFONE":
+        let regexTelefone =
+          /^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/;
+        if (regexTelefone.test(chavePix)) {
+          this.chavesPix.telefone = chavePix;
+          return "Chave Pix TELEFONE criada com sucesso";
+        } else {
+          throw new Error("Erro: telefone Inválido");
+        }
+
+      default:
+        return "Chave inexistente";
+    }
+  }
+
   criarConta(agencia, conta, saldo) {
     if (agencia.length === 4 && conta.length === 5 && saldo > 0) {
       this.#agencia = agencia;
@@ -46,7 +89,7 @@ class Conta {
   sacar(valor) {
     if (valor > 0 && typeof valor === "number") {
       if (this.#saldo - valor > 0) {
-        const saldo = (this.#saldo - valor);
+        const saldo = this.#saldo - valor;
         this.setSaldo(saldo);
       } else {
         throw new Error("valor inválido para saque");
@@ -59,13 +102,37 @@ class Conta {
   depositar(valor) {
     if (valor > 0 && typeof valor === "number") {
       if (this.#saldo + valor > 0) {
-        const saldo = (this.#saldo + valor);
+        const saldo = this.#saldo + valor;
         this.setSaldo(saldo);
       } else {
         throw new Error("valor inválido para depósito");
       }
     } else {
       throw new Error("valor inválido para depósito");
+    }
+  }
+
+  transferir(valor, agencia, conta) {
+    let contaValida = Conta.listaContas.find(contaReceptora => {
+      let numeroContaReceptora = contaReceptora.getConta
+      let numeroAgenciaReceptora =  contaReceptora.getAgencia
+      return numeroContaReceptora === conta && numeroAgenciaReceptora === agencia
+    })
+
+    if (!contaValida){
+      throw new Error("Conta não encontrada")
+    }
+
+    if (valor < 0) {
+      throw new Error ("Valor inválido para trnasferência")
+    }
+
+    if(this.#saldo - valor > 0) {
+      const saldoAtualizado = this.#saldo - valor;
+      this.setSaldo(saldoAtualizado)
+      const saldoContaReceptora = contaValida.getSaldo + valor
+      contaValida.setSaldo(saldoContaReceptora)
+      return "Transferência realizada"
     }
   }
 }
--- exercicios/projeto/Conta/Conta.test.js
+++ exercicios/projeto/Conta/Conta.test.js
@@ -1,75 +1,134 @@
-const Conta = require("./Conta")
+const Conta = require("./Conta");
 
 describe("Teste da classe Conta", () => {
-    test("Verificar se a instância Conta está sendo criada", () => {
-        const conta = new Conta();
-        expect(conta instanceof Conta).toBe(true);
-    })
+  test("Verificar se a instância Conta está sendo criada", () => {
+    const conta = new Conta();
+    expect(conta instanceof Conta).toBe(true);
+  });
 
-    test("Instanciar conta com valores válidos", () => {
-        const conta = new Conta("1234", "12345", 1000);
-        expect(conta.getAgencia).toBe("1234");
-        expect(conta.getConta).toBe("12345");
-        expect(conta.getSaldo).toBe(1000);
-    })
+  test("Instanciar conta com valores válidos", () => {
+    const conta = new Conta("1234", "12345", 1000);
+    expect(conta.getAgencia).toBe("1234");
+    expect(conta.getConta).toBe("12345");
+    expect(conta.getSaldo).toBe(1000);
+  });
 
-    test("retorna mensagem de sucesso ao criar conta", () => {
-        const conta = new Conta();
-        expect(conta.criarConta("1234", "12345", 1000)).toBe("Conta criada com sucesso")
-        expect(conta.getAgencia).toBe("1234");
-        expect(conta.getConta).toBe("12345");
-        expect(conta.getSaldo).toBe(1000);
-    })
+  test("retorna mensagem de sucesso ao criar conta", () => {
+    const conta = new Conta();
+    expect(conta.criarConta("1234", "12345", 1000)).toBe(
+      "Conta criada com sucesso"
+    );
+    expect(conta.getAgencia).toBe("1234");
+    expect(conta.getConta).toBe("12345");
+    expect(conta.getSaldo).toBe(1000);
+  });
 
-    test("retorna mensagem de ERRO ao criar conta", () => {
-        const conta = new Conta();
-        expect(() => conta.criarConta("12345", "123", 1000)).toThrow("Erro no cadastro, dados inválidos")
-    })
+  test("retorna mensagem de ERRO ao criar conta", () => {
+    const conta = new Conta();
+    expect(() => conta.criarConta("12345", "123", 1000)).toThrow(
+      "Erro no cadastro, dados inválidos"
+    );
+  });
 
-    test("retorna sucesso ao sacar 100 da conta", () => {
-        const conta = new Conta();
-        conta.criarConta("1234", "12345", 1000)
-        
-        conta.sacar(100)
-        expect(conta.getSaldo).toBe(900)
-    })
+  test("retorna sucesso ao sacar 100 da conta", () => {
+    const conta = new Conta();
+    conta.criarConta("1234", "12345", 1000);
 
-    test("retorna mensagem de erro ao sacar -100 da conta", () => {
-        const conta = new Conta();
-        conta.criarConta("1234", "12345", 1000)
-        
-        expect(() => conta.sacar(-100)).toThrow("valor inválido para saque")
-        expect(conta.getSaldo).toBe(1000)
-   
-    })
+    conta.sacar(100);
+    expect(conta.getSaldo).toBe(900);
+  });
 
-    
-    test("retorna sucesso ao depositar 100 na conta", () => {
-        const conta = new Conta();
-        conta.criarConta("1234", "12345", 1000)
-        
-        conta.depositar (100)   
-        expect(conta.getSaldo).toBe(1100)
-    })
+  test("retorna mensagem de erro ao sacar -100 da conta", () => {
+    const conta = new Conta();
+    conta.criarConta("1234", "12345", 1000);
 
-    test("retorna mensagem de erro ao depositar -100 da conta", () => {
-        const conta = new Conta();
-        conta.criarConta("1234", "12345", 1000)
-        
-        expect(() => conta.depositar(-100)).toThrow("valor inválido para depósito")
-        expect(conta.getSaldo).toBe(1000)
-   
-    })
+    expect(() => conta.sacar(-100)).toThrow("valor inválido para saque");
+    expect(conta.getSaldo).toBe(1000);
+  });
 
-    test("retorna mensagem de erro ao depositar valor não numérico da conta", () => {
-        const conta = new Conta();
-        conta.criarConta("1234", "12345", 1000)
-        
-        expect(() => conta.depositar(" ")).toThrow("valor inválido para depósito")
-        expect(conta.getSaldo).toBe(1000)
-   
-    })
+  test("retorna sucesso ao depositar 100 na conta", () => {
+    const conta = new Conta();
+    conta.criarConta("1234", "12345", 1000);
 
+    conta.depositar(100);
+    expect(conta.getSaldo).toBe(1100);
+  });
 
+  test("retorna mensagem de erro ao depositar -100 da conta", () => {
+    const conta = new Conta();
+    conta.criarConta("1234", "12345", 1000);
 
-})
+    expect(() => conta.depositar(-100)).toThrow("valor inválido para depósito");
+    expect(conta.getSaldo).toBe(1000);
+  });
+
+  test("retorna mensagem de erro ao depositar valor não numérico da conta", () => {
+    const conta = new Conta();
+    conta.criarConta("1234", "12345", 1000);
+
+    expect(() => conta.depositar(" ")).toThrow("valor inválido para depósito");
+    expect(conta.getSaldo).toBe(1000);
+  });
+
+  test("criar uma chave pix por cpf com sucesso", () => {
+    const conta = new Conta();
+    const operacao = conta.criarChavePix("40812545878", "CPF");
+
+    expect(operacao).toBe("Chave Pix CPF criada com sucesso");
+    expect(conta.chavesPix.cpf).toBe("40812545878");
+  });
+
+  test("retornar mensagem de erro ao tentar cadastrar chave pix com cpf invalido", () => {
+    const conta = new Conta();
+    const operacao = conta.criarChavePix("4081878", "CPF");
+
+    expect(() => operacao).toThrow("Erro: CPF Inválido");
+  });
+
+  test("criar uma chave pix por e-mail com sucesso", () => {
+    const conta = new Conta();
+    const operacao = conta.criarChavePix("roxanie@email.com.br", "EMAIL");
+
+    expect(operacao).toBe("Chave Pix EMAIL criada com sucesso");
+    expect(conta.chavesPix.email).toBe("roxanie@email.com.br");
+  });
+
+  test("retornar mensagem de erro ao tentar cadastrar chave pix com email invalido", () => {
+    const conta = new Conta();
+    const operacao = conta.criarChavePix("roxanie.com", "EMAIL");
+
+    expect(() => operacao).toThrow("Erro: email Inválido");
+  });
+
+  test("criar uma chave pix por telefone com sucesso", () => {
+    const conta = new Conta();
+    const operacao = conta.criarChavePix("(11)917383003", "TELEFONE");
+
+    expect(operacao).toBe("Chave Pix TELEFONE criada com sucesso");
+    expect(conta.chavesPix.telefone).toBe("(11)917383003");
+  });
+
+  test("retornar mensagem de erro ao tentar cadastrar chave pix com telefone invalido", () => {
+    const conta = new Conta();
+    const operacao = conta.criarChavePix("917383003", "TELEFONE");
+
+    expect(() => operacao).toThrow("Erro: telefone Inválido");
+  });
+
+  test("retorna sucesso ao fazer uma transferência com valor válido, saldo suficiente, dados válidos", () => {
+    const contaEmissor = new Conta();
+    const contaReceptor = new Conta();
+
+    contaEmissor.criarConta("0001", "12345", 1000);
+    contaReceptor.criarConta("0001", "78945", 500);
+
+    const operacao = contaEmissor.transferir(100, "0001", "78945");
+
+    expect(operacao).toBe("Transferência realizada");
+    expect(contaEmissor.getSaldo).toBe(900);
+    expect(contaReceptor.getSaldo).toBe(600);
+
+    contaEmissor.destruir()
+    contaReceptor.destruir()
+  });
+});
