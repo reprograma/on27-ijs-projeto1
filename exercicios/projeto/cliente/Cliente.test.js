fb88f3d208cee3cdfeabd79123ecc0d010243457
Heloiza Mendes <helocursos@gmail.com>
2023-11-03 15:05:31 -0300

    refatoração

--- exercicios/projeto/cliente/Cliente.test.js
+++ exercicios/projeto/cliente/Cliente.test.js
@@ -1,22 +1,24 @@
-const Cliente = require('./Cliente')
-const Conta = require('../Conta/Conta')
+const Cliente = require("./Cliente");
+const Conta = require("../Conta/Conta");
 
-
-describe("Teste da classe Cliente", () => {
-    test("Verificar se a instância Client está sendo criada", () => {
+describe("Teste da classe Cliente", ()=>{
+    test("verificar se instancia Cliente está sendo criada", ()=>{
         const cliente = new Cliente();
-        // expect(cliente).toBeInstanceof(Cliente);
+        //alternativa: expect(cliente).toBeInstanceOf(Cliente)
         expect(cliente instanceof Cliente).toBe(true);
-    })
+    });
 
-    test("Cadastrar cliente com dados válidos", () => {
+    test("cadastrar cliente com dados válidos", ()=>{
         const cliente = new Cliente();
         const conta = new Conta();
-        expect(cliente.registrar("Analu", "2323123232323242", 5000, conta)).toBe("Cliente Cadastrado")
+
+        expect(cliente.registrar("Analu", "121564943121", 5000, conta)).toBe("Cliente Cadastrado")
     })
 
-    test("Cadastrar cliente com dados invalidos", () => {
-        const cliente = new Cliente()
-        expect(() => cliente.registrar("Analu", "123232323", 5000, "não conta")).toThrow("Erro no cadastro, dados invalidos")
+    test("retorne mensagem de erro quando tenta cadastrar cliente sem uma conta", ()=>{
+        const cliente = new Cliente();
+
+        expect(() => cliente.registrar("Analu", "121564943121", 5000, "nao conta")).toThrow("Erro no cadastro, dados inválidos")
+    
     })
 })
