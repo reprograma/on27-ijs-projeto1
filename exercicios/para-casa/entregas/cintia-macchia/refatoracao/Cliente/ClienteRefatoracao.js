e4e6c3cfa266f6a15ffdf283b2e9b5c5ffe7843e
Cintia Macchia <macchiapaola@hotmail.com>
2023-11-14 19:45:33 -0300

    Add arquivos cliente refatorados

---
+++ exercicios/para-casa/entregas/cintia-macchia/refatoracao/Cliente/ClienteRefatoracao.js
@@ -0,0 +1,22 @@
+ const { Conta }  = require("../Conta/Conta")
+ 
+ class Cliente{
+    #cpf;
+    #renda;
+    #conta;
+
+    registrar(nome, cpf, renda, conta){
+        if(conta instanceof Conta){
+        this.nome =  nome,
+        this.#cpf = cpf,
+        this.#renda = renda,
+        this.#conta = conta 
+
+        return "Cliente Cadastrado";
+        } else {
+        throw new Error("Erro no cadastro, dados inválidos")
+        }
+    }
+}
+
+module.exports = { Cliente }
